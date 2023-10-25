77ab4d5b44424f47e722ba8e63f62487cf07c951
Pavan Kumar Reddy Onteddu <60224774+pa11teddu@users.noreply.github.com>
2023-10-25 01:39:10 -0700

    Update Duplicate_code_example.py

--- Duplicate_code_example.py
+++ Duplicate_code_example.py
@@ -3,10 +3,7 @@ def calculate_area_and_perimeter(length, width):
     perimeter = 2 * (length + width)
     return area, perimeter
 
-def calculate_volume(length, width, height):
+def calculate_volume_and_surface_area(length, width, height):
     volume = length * width * height
-    return volume
-
-def calculate_surface_area(length, width, height):
     surface_area = 2 * (length * width + length * height + width * height)
-    return surface_area
+    return volume, surface_area

4ab467e26f11a57525780e5de0911c9ee5db6891
Pavan Kumar Reddy Onteddu <60224774+pa11teddu@users.noreply.github.com>
2023-10-25 01:52:08 -0700

    Update Duplicate_code_example.py

--- Duplicate_code_example.py
+++ Duplicate_code_example.py
@@ -15,7 +15,7 @@ def calculate_volume(length, width, height):
 
 
 def calculate_surface_area(length, width, height):
-    surface_area = 2 * (length * width + length, height + height, width)
+    surface_area = 2 * (length * width + length * height + width * height)
     return surface_area
 
 

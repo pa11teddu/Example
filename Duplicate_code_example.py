cf1dedd62a0dd7da3607b372a3b355b06c0d53a3
Pavan Kumar Reddy Onteddu <60224774+pa11teddu@users.noreply.github.com>
2023-10-28 02:56:06 -0700

    Update Duplicate_code_example.py

--- Duplicate_code_example.py
+++ Duplicate_code_example.py
@@ -10,24 +10,10 @@ def calculate_volume(length, width, height):
 def calculate_surface_area(length, width, height):
   return 2 * (length * width + length * height + width * height)
 
-# Extract the duplicate code from the `calculate_area_perimeter()` function into a
-# separate function.
 def calculate_area_and_perimeter(length, width):
   area = calculate_area(length, width)
   perimeter = calculate_perimeter(length, width)
   return area, perimeter
 
-# Refactor the `calculate_area_perimeter()` function to call the new
-# `calculate_area_and_perimeter()` function.
 def calculate_area_perimeter(length, width):
-  """Calculates the area and perimeter of a rectangle.
-
-  Args:
-    length: The length of the rectangle.
-    width: The width of the rectangle.
-
-  Returns:
-    A tuple containing the area and perimeter of the rectangle.
-  """
   return calculate_area_and_perimeter(length, width)
-
